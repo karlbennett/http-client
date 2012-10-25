6e6ec68113205ce5099adadbf4caeabd57db670d
Karl Bennett <where_1ts@yahoo.com>
2012-10-25 23:31:17 +0100

    Overloaded all the HTTP methods with version that take URL and request objects. Also created test templates for all the overloads.

--- src/main/java/http/Client.java
+++ src/main/java/http/Client.java
@@ -1,5 +1,10 @@
 package http;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 /**
  * @author Karl Bennett
  */
@@ -19,9 +24,29 @@ public class Client {
         return getInstance().options(url);
     }
 
+    public static Response OPTIONS(URL url) {
+
+        return getInstance().options(url);
+    }
+
+    public static Response OPTIONS(Request request) {
+
+        return getInstance().options(request);
+    }
+
     public static Response GET(String url) {
 
         return getInstance().get(url);
+    }
+
+    public static Response GET(URL url) {
+
+        return getInstance().get(url);
+    }
+
+    public static Response GET(Request request) {
+
+        return getInstance().get(request);
     }
 
     public static Response HEAD(String url) {
@@ -29,9 +54,29 @@ public class Client {
         return getInstance().head(url);
     }
 
+    public static Response HEAD(URL url) {
+
+        return getInstance().head(url);
+    }
+
+    public static Response HEAD(Request request) {
+
+        return getInstance().head(request);
+    }
+
     public static Response POST(String url) {
 
         return getInstance().post(url);
+    }
+
+    public static Response POST(URL url) {
+
+        return getInstance().post(url);
+    }
+
+    public static Response POST(Request request) {
+
+        return getInstance().post(request);
     }
 
     public static Response PUT(String url) {
@@ -39,9 +84,29 @@ public class Client {
         return getInstance().put(url);
     }
 
+    public static Response PUT(URL url) {
+
+        return getInstance().put(url);
+    }
+
+    public static Response PUT(Request request) {
+
+        return getInstance().put(request);
+    }
+
     public static Response DELETE(String url) {
 
         return getInstance().delete(url);
+    }
+
+    public static Response DELETE(URL url) {
+
+        return getInstance().delete(url);
+    }
+
+    public static Response DELETE(Request request) {
+
+        return getInstance().delete(request);
     }
 
     public static Response TRACE(String url) {
@@ -49,9 +114,39 @@ public class Client {
         return getInstance().trace(url);
     }
 
+    public static Response TRACE(URL url) {
+
+        return getInstance().trace(url);
+    }
+
+    public static Response TRACE(Request request) {
+
+        return getInstance().trace(request);
+    }
+
     public static Response CONNECT(String url) {
 
         return getInstance().connect(url);
+    }
+
+    public static Response CONNECT(URL url) {
+
+        return getInstance().connect(url);
+    }
+
+    public static Response CONNECT(Request request) {
+
+        return getInstance().connect(request);
+    }
+
+    public static Configuration CONFIG() {
+
+        return getInstance().config();
+    }
+
+    public static void CONFIG(Configuration configuration) {
+
+        getInstance().config(configuration);
     }
 
 
@@ -61,49 +156,186 @@ public class Client {
 
     public Response options(String url) {
 
+        return options(new Request(url));
+    }
+
+    public Response options(URL url) {
+
+        return options(new Request(url));
+    }
+
+    public Response options(Request request) {
+
         return null;
     }
 
     public Response get(String url) {
 
+        return get(new Request(url));
+    }
+
+    public Response get(URL url) {
+
+        return get(new Request(url));
+    }
+
+    public Response get(Request request) {
+
         return null;
     }
-    
+
     public Response head(String url) {
+
+        return head(new Request(url));
+    }
+
+    public Response head(URL url) {
+
+        return head(new Request(url));
+    }
+
+    public Response head(Request request) {
 
         return null;
     }
 
     public Response post(String url) {
 
+        return post(new Request(url));
+    }
+
+    public Response post(URL url) {
+
+        return post(new Request(url));
+    }
+
+    public Response post(Request request) {
+
         return null;
     }
 
     public Response put(String url) {
+
+        return put(new Request(url));
+    }
+
+    public Response put(URL url) {
+
+        return put(new Request(url));
+    }
+
+    public Response put(Request request) {
 
         return null;
     }
 
     public Response delete(String url) {
 
+        return delete(new Request(url));
+    }
+
+    public Response delete(URL url) {
+
+        return delete(new Request(url));
+    }
+
+    public Response delete(Request request) {
+
         return null;
     }
 
     public Response trace(String url) {
+
+        return trace(new Request(url));
+    }
+
+    public Response trace(URL url) {
+
+        return trace(new Request(url));
+    }
+
+    public Response trace(Request request) {
 
         return null;
     }
 
     public Response connect(String url) {
 
+        return connect(new Request(url));
+    }
+
+    public Response connect(URL url) {
+
+        return connect(new Request(url));
+    }
+
+    public Response connect(Request request) {
+
         return null;
     }
 
+
+    public Configuration config() {
+
+        return null;
+    }
+
+    public void config(Configuration configuration) {
+
+    }
+
+
     public static class Request {
 
+        private static URL quietUrl(String url) {
+
+            try {
+
+                return new URI(url).toURL();
+
+            } catch (MalformedURLException e) {
+
+                throw new URLException(e);
+
+            } catch (URISyntaxException e) {
+
+                throw new URLException(e);
+            }
+        }
+
+
+        public Request(String url) {
+
+            this(quietUrl(url));
+        }
+
+        public Request(URL url) {
+        }
     }
 
     public static class Response {
 
+    }
+
+    public static class Configuration {
+
+    }
+
+    public static class URLException extends IllegalArgumentException {
+
+        public URLException() {
+        }
+
+        public URLException(String s) {
+            super(s);
+        }
+
+        public URLException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public URLException(Throwable cause) {
+            super(cause);
+        }
     }
 }
--- src/test/java/http/ClientTest.java
+++ src/test/java/http/ClientTest.java
@@ -4,107 +4,323 @@ import static http.Client.*;
 
 import org.junit.Test;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 /**
  * @author Karl Bennett
  */
 public class ClientTest {
 
-    private static final String URL = "";
+    private static final String URL_STRING = "";
+
+    private static final URL URL;
+    static {
+
+        try {
+
+            URL = new URI(URL_STRING).toURL();
+
+        } catch (MalformedURLException e) {
+
+            throw new IllegalStateException(e);
+
+        } catch (URISyntaxException e) {
+
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private static final Request REQUEST = new Request(URL);
 
 
     @Test
-    public void testOPTIONS() throws Exception {
+    public void testOPTIONSUrlString() throws Exception {
+
+        OPTIONS(URL_STRING);
+    }
+
+    @Test
+    public void testOPTIONSUrl() throws Exception {
 
         OPTIONS(URL);
     }
 
     @Test
-    public void testGET() throws Exception {
+    public void testOPTIONSRequest() throws Exception {
+
+        OPTIONS(REQUEST);
+    }
+
+    @Test
+    public void testGETUrlString() throws Exception {
+
+        GET(URL_STRING);
+    }
+
+    @Test
+    public void testGETUrl() throws Exception {
 
         GET(URL);
     }
 
     @Test
-    public void testHEAD() throws Exception {
+    public void testGETRequest() throws Exception {
+
+        GET(REQUEST);
+    }
+
+    @Test
+    public void testHEADUrlString() throws Exception {
+
+        HEAD(URL_STRING);
+    }
+
+    @Test
+    public void testHEADUrl() throws Exception {
 
         HEAD(URL);
     }
 
     @Test
-    public void testPOST() throws Exception {
+    public void testHEADRequest() throws Exception {
+
+        HEAD(REQUEST);
+    }
+
+    @Test
+    public void testPOSTUrlString() throws Exception {
+
+        POST(URL_STRING);
+    }
+
+    @Test
+    public void testPOSTUrl() throws Exception {
 
         POST(URL);
     }
 
     @Test
-    public void testPUT() throws Exception {
+    public void testPOSTRequest() throws Exception {
 
-        PUT(URL);
+        POST(REQUEST);
     }
 
     @Test
-    public void testDELETE() throws Exception {
+    public void testPUTUrlString() throws Exception {
+
+        PUT(URL_STRING);
+    }
+
+    @Test
+    public void testPUTUrl() throws Exception {
+
+        PUT(URL_STRING);
+    }
+
+    @Test
+    public void testPUTRequest() throws Exception {
+
+        PUT(URL_STRING);
+    }
+
+    @Test
+    public void testDELETEUrlString() throws Exception {
+
+        DELETE(URL_STRING);
+    }
+
+    @Test
+    public void testDELETEUrl() throws Exception {
 
         DELETE(URL);
     }
 
     @Test
-    public void testTRACE() throws Exception {
+    public void testDELETERequest() throws Exception {
+
+        DELETE(REQUEST);
+    }
+
+    @Test
+    public void testTRACEUrlString() throws Exception {
+
+        TRACE(URL_STRING);
+    }
+
+    @Test
+    public void testTRACEUrl() throws Exception {
 
         TRACE(URL);
     }
 
     @Test
-    public void testCONNECT() throws Exception {
+    public void testTRACERequest() throws Exception {
+
+        TRACE(REQUEST);
+    }
+
+    @Test
+    public void testCONNECTUrlString() throws Exception {
+
+        CONNECT(URL_STRING);
+    }
+
+    @Test
+    public void testCONNECTUrl() throws Exception {
 
         CONNECT(URL);
     }
 
     @Test
-    public void testOptions() throws Exception {
+    public void testCONNECTRequest() throws Exception {
+
+        CONNECT(REQUEST);
+    }
+
+    @Test
+    public void testOptionsUrlString() throws Exception {
+
+        new Client().options(URL_STRING);
+    }
+
+    @Test
+    public void testOptionsUrl() throws Exception {
 
         new Client().options(URL);
     }
 
     @Test
-    public void testGet() throws Exception {
+    public void testOptionsRequest() throws Exception {
+
+        new Client().options(REQUEST);
+    }
+
+    @Test
+    public void testGetUrlString() throws Exception {
+
+        new Client().get(URL_STRING);
+    }
+
+    @Test
+    public void testGetUrl() throws Exception {
 
         new Client().get(URL);
     }
 
     @Test
-    public void testHead() throws Exception {
+    public void testGetRequest() throws Exception {
+
+        new Client().get(REQUEST);
+    }
+
+    @Test
+    public void testHeadUrlString() throws Exception {
+
+        new Client().head(URL_STRING);
+    }
+
+    @Test
+    public void testHeadUrl() throws Exception {
 
         new Client().head(URL);
     }
 
     @Test
-    public void testPost() throws Exception {
+    public void testHeadRequest() throws Exception {
+
+        new Client().head(REQUEST);
+    }
+
+    @Test
+    public void testPostUrlString() throws Exception {
+
+        new Client().post(URL_STRING);
+    }
+
+    @Test
+    public void testPostUrl() throws Exception {
 
         new Client().post(URL);
     }
 
     @Test
-    public void testPut() throws Exception {
+    public void testPostRequest() throws Exception {
+
+        new Client().post(REQUEST);
+    }
+
+    @Test
+    public void testPutUrlString() throws Exception {
+
+        new Client().put(URL_STRING);
+    }
+
+    @Test
+    public void testPutUrl() throws Exception {
 
         new Client().put(URL);
     }
 
     @Test
-    public void testDelete() throws Exception {
+    public void testPutRequest() throws Exception {
+
+        new Client().put(REQUEST);
+    }
+
+    @Test
+    public void testDeleteUrlString() throws Exception {
+
+        new Client().delete(URL_STRING);
+    }
+
+    @Test
+    public void testDeleteUrl() throws Exception {
 
         new Client().delete(URL);
     }
 
     @Test
-    public void testTrace() throws Exception {
+    public void testDeleteRequest() throws Exception {
+
+        new Client().delete(REQUEST);
+    }
+
+    @Test
+    public void testTraceUrlString() throws Exception {
+
+        new Client().trace(URL_STRING);
+    }
+
+    @Test
+    public void testTraceUrl() throws Exception {
 
         new Client().trace(URL);
     }
 
     @Test
-    public void testConnect() throws Exception {
+    public void testTraceRequest() throws Exception {
+
+        new Client().trace(REQUEST);
+    }
+
+    @Test
+    public void testConnectUrlString() throws Exception {
+
+        new Client().connect(URL_STRING);
+    }
+
+    @Test
+    public void testConnectUrl() throws Exception {
 
         new Client().connect(URL);
+    }
+
+    @Test
+    public void testConnectRequest() throws Exception {
+
+        new Client().connect(REQUEST);
     }
 }
