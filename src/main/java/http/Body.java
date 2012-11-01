7dfd56ad6f02d99e48935b5c3c9c5bc3ef457578
Karl Bennett <where_1ts@yahoo.com>
2012-11-01 22:51:20 +0000

    Fleshed out the Body class.

--- src/main/java/http/Body.java
+++ src/main/java/http/Body.java
@@ -1,7 +1,51 @@
 package http;
 
+import java.util.Collection;
+import java.util.Collections;
+
 /**
  * @author Karl Bennett
  */
-public class Body {
+public class Body<S> {
+
+    public Collection<Parameter> getParameters() {
+
+        return null;
+    }
+
+    public Parameter getParameter(String name) {
+
+        return null;
+    }
+
+    public void setParameters(Collection<Parameter> parameters) {
+
+    }
+
+    public void addParameter(String name, Object value) {
+
+        addParameter(new Parameter(name, Collections.singletonList(value)));
+    }
+
+    public void addParameter(Parameter parameter) {
+
+    }
+
+    public S getContent() {
+
+        return null;
+    }
+
+    public void setContent(S stream) {
+
+    }
+
+    public <T> T getContent(Class<T> type) {
+
+        return null;
+    }
+
+    public <T> void setContent(Class<T> type, T content) {
+
+    }
 }
