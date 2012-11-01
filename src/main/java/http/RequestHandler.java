3188e507dc5ad9c2a319d3d417fb5694cc76affe
Karl Bennett <where_1ts@yahoo.com>
2012-11-01 22:57:47 +0000

    Created a request handler interface that will be implemented by the different http.Client backends.

---
+++ src/main/java/http/RequestHandler.java
@@ -0,0 +1,13 @@
+package http;
+
+import java.io.InputStream;
+import java.util.Collection;
+
+/**
+ * @author Karl Bennett
+ */
+interface RequestHandler {
+
+    public Response handleRequest(String method, Collection<Header> headers, Collection<Parameter> parameters,
+                                  Collection<Cookie> cookies, Body<InputStream> body);
+}
