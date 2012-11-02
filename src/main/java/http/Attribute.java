83e13c0a57c0de01cedc36820cd9cccae7ad2218
Karl Bennett <where_1ts@yahoo.com>
2012-11-02 22:40:57 +0000

    Added coments for the Attribute class.

--- src/main/java/http/Attribute.java
+++ src/main/java/http/Attribute.java
@@ -3,6 +3,8 @@ package http;
 import java.util.List;
 
 /**
+ * Represents a generic HTTP attribute with a name and multiple values.
+ *
  * @author Karl Bennett
  */
 public class Attribute {
@@ -11,16 +13,28 @@ public class Attribute {
     private final List<Object> values;
 
 
+    /**
+     * Create an {@code Attribute} with a name and values.
+     *
+     * @param name the name of the attribute.
+     * @param values the values for the attribute.
+     */
     public Attribute(String name, List<Object> values) {
 
         this.name = name;
         this.values = values;
     }
 
+    /**
+     * @return the attributes name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return the attributes values.
+     */
     public List<Object> getValues() {
         return values;
     }
