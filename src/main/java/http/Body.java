a73dca362da8d3aaba1f89fe74d24638e3634f53
Karl Bennett <where_1ts@yahoo.com>
2012-11-02 22:41:20 +0000

    Added comments for the Body class.

--- src/main/java/http/Body.java
+++ src/main/java/http/Body.java
@@ -1,42 +1,44 @@
 package http;
 
-import java.util.Collection;
-import java.util.Collections;
-
 /**
+ * Represent the body of an HTTP message, be this a {@link Request} or {@link Response}. The type of content the body
+ * contains can be set on instantiation.
+ *
+ * @param <T> the type of content the body contains e.g. {@code String}, {@code Collection&lt;Parameter&gt;},
+ *            {@code InputStream}...
  * @author Karl Bennett
  */
-public class Body<S> {
+public class Body<T> {
 
-    public Collection<Parameter> getParameters() {
+    /**
+     * Create a new {@code Body} containing content of the set generic type.
+     *
+     * @param content the content of the body.
+     */
+    public Body(T content) {
+
+    }
+
+    /**
+     * @return return the content of the body.
+     */
+    public T getContent() {
 
         return null;
     }
 
-    public Parameter getParameter(String name) {
+    /**
+     * Return the content of the body as the supplied type. This uses the clients converters to convert the current
+     * type of the body into the requested type.
+     *
+     * @param type the type that the body content should be converted into.
+     * @param <T>  the generic type that the body content should be converted into.
+     * @return the content of the body as the requested type.
+     * @throws IllegalStateException if no converter could be found that supports the body content type as input and the
+     *                               requested type as output.
+     */
+    public <T> T getContent(Class<T> type) {
 
         return null;
-    }
-
-    public void setParameters(Collection<Parameter> parameters) {
-
-    }
-
-    public void addParameter(String name, Object value) {
-
-        addParameter(new Parameter(name, Collections.singletonList(value)));
-    }
-
-    public void addParameter(Parameter parameter) {
-
-    }
-
-    public S getContent() {
-
-        return null;
-    }
-
-    public void setContent(S stream) {
-
     }
 }
