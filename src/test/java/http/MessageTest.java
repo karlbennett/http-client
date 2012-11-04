fd168f1e134e4628adb4907253bd0a4654ce764e
Karl Bennett <where_1ts@yahoo.com>
2012-11-04 23:25:27 +0000

    Created the message test class template.

---
+++ src/test/java/http/MessageTest.java
@@ -0,0 +1,95 @@
+package http;
+
+import org.junit.Test;
+
+/**
+ * @author Karl Bennett
+ */
+public class MessageTest {
+
+    @Test
+    public void testGetHeaders() throws Exception {
+
+
+    }
+
+    @Test
+    public void testGetHeader() throws Exception {
+
+    }
+
+    @Test
+    public void testSetHeaders() throws Exception {
+
+    }
+
+    @Test
+    public void testAddHeaderWithNameAndValue() throws Exception {
+
+    }
+
+    @Test
+    public void testAddHeader() throws Exception {
+
+    }
+
+    @Test
+    public void testGetParameters() throws Exception {
+
+    }
+
+    @Test
+    public void testGetParameter() throws Exception {
+
+    }
+
+    @Test
+    public void testSetParameters() throws Exception {
+
+    }
+
+    @Test
+    public void testAddParameterWithNameAndValue() throws Exception {
+
+    }
+
+    @Test
+    public void testAddParameter() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCookies() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCookie() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCookies() throws Exception {
+
+    }
+
+    @Test
+    public void testAddCookieWithNameAndValue() throws Exception {
+
+    }
+
+    @Test
+    public void testAddCookie() throws Exception {
+
+    }
+
+    @Test
+    public void testGetBody() throws Exception {
+
+    }
+
+    @Test
+    public void testSetBody() throws Exception {
+
+    }
+}
