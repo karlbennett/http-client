7aa00314e9d8112b0197b94a1d57d19da17a561d
Karl Bennett <where_1ts@yahoo.com>
2012-11-04 21:32:02 +0000

    Created some convenience constructors for the response and added comments.

--- src/main/java/http/Response.java
+++ src/main/java/http/Response.java
@@ -1,17 +1,65 @@
 package http;
 
+import java.util.Collection;
+
 /**
  * Represents an {@code HTTP} response and provides access to all the standard response components.
  *
  * @author Karl Bennett
  */
-public class Response extends Message {
+public class Response<T> extends Message<T> {
 
+    /**
+     * Create a {@code Response} with the supplied HTTP status code.
+     *
+     * @param status the status code for the response.
+     */
+    public Response(int status) {
+    }
+
+    /**
+     * Create a {@code Response} with the supplied HTTP status code and body.
+     *
+     * @param status the status code for the response.
+     * @param body the body of te response.
+     */
+    public Response(int status, T body) {
+    }
+
+    /**
+     * Create a {@code Response} with the supplied HTTP status code, headers, and body.
+     *
+     * @param status the status code for the response.
+     * @param headers the headers for the response.
+     * @param body the body of te response.
+     */
+    public Response(int status, Collection<Header> headers, T body) {
+    }
+
+    /**
+     * Create a {@code Response} with the supplied HTTP status code, headers, and body.
+     *
+     * @param status the status code for the response.
+     * @param headers the headers for the response.
+     * @param cookies the cookies for the response.
+     * @param body the body of te response.
+     */
+    public Response(int status, Collection<Header> headers, Collection<Cookie> cookies, T body) {
+    }
+
+    /**
+     * @return the status code for the response.
+     */
     public int getStatus() {
 
         return 0;
     }
 
+    /**
+     * Set the status code for the response.
+     *
+     * @param status the status code for the response.
+     */
     public void setStatus(int status) {
 
     }
