09269a1933051c40842130dde63f254ac5934d24
Karl Bennett <where_1ts@yahoo.com>
2012-11-04 15:34:43 +0000

    Added comments to the Header class.

--- src/main/java/http/Header.java
+++ src/main/java/http/Header.java
@@ -3,10 +3,18 @@ package http;
 import java.util.List;
 
 /**
+ * Represent an HTTP header which is made up of a name and possibly multiple values.
+ *
  * @author Karl Bennett
  */
 public class Header extends Attribute {
 
+    /**
+     * Create a new {@code Header} with the supplied name and values.
+     *
+     * @param name the name of the header.
+     * @param values the values for the header.
+     */
     public Header(String name, List<Object> values) {
 
         super(name, values);
