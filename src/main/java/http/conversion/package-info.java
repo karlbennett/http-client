eb0b5425156982214820c248f81bd40b0dbd2d72
Karl Bennett <where_1ts@yahoo.com>
2012-11-04 23:06:13 +0000

    Created JavaDoc package files.

---
+++ src/main/java/http/conversion/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package contains the {@link http.conversion.Converter} interface an all the default implementations for
+ * converting common types.
+ */
+package http.conversion;
---
+++ src/main/java/http/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This is the main package for the http-client and contains the main {@link http.Client} class which can be imported on
+ * it's own to provide most of the API's functionality. The other classes within this package more fine grained
+ * construction of a {@link http.Client.Request}.
+ */
+package http;
