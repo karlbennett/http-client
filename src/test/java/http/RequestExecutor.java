56f723458edc6024c52001f801d6344e006f397e
Karl Bennett <where_1ts@yahoo.com>
2012-11-18 19:33:36 +0000

    Removed the RequestExecutor.getMethod and RequestExecutor.getMethodType methods because the method type isn't really related to the executor, it should just execute the request.

--- src/test/java/http/RequestExecutor.java
+++ src/test/java/http/RequestExecutor.java
@@ -3,25 +3,25 @@ package http;
 import static http.Client.Response;
 
 /**
- * This interface is used to allow the switching of request methods within the same peace of code. It used to allow code
- * reuse within the HTTP request methods tests which are all very similar.
+ * This interface is used to allow the switching of request methods within the same peace of code. It's used to allow
+ * code reuse within the HTTP request methods tests which are all very similar.
  *
- * @param <T> the type of input that will be used for the request method, this will usually be the {@link String} or
- * {@link http.Client.Request} type.
+ * @param <T> the type of input that will be used for the request method, this will usually be the {@link String},
+ * {@link java.net.URL}, or {@link http.Client.Request} type.
  *
  * @author Karl Bennett
  */
 public interface RequestExecutor<T> {
 
     /**
-     * @return the method string for the contained request e.g. "GET", "POST", "PUT", "DELETE"...
+     * Denotes the type of a request method, that is static method like {@link Client#GET(String)},
+     * {@link Client#POST(String)}, {@link Client#PUT(String)}, or an instance method like {@link Client#get(String)},
+     * {@link Client#post(String)}, {@link Client#put(String)}.
      */
-    public String getMethod();
-
-    /**
-     * @return whether a static or instance method is being called.
-     */
-    public String getMethodType();
+    public static enum METHOD_TYPE {
+        STATIC,
+        INSTANCE
+    }
 
     /**
      * Execute the wrapped request method method. This could be a static or instance method e.g.
