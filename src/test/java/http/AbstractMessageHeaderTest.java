72b40253a682e14f04eab7e496a73fa6cead5d99
Karl Bennett <where_1ts@yahoo.com>
2012-12-02 16:54:30 +0000

    Created new AbstractMessageHeaderTest that sets the propertyExecutor as a HEader.

---
+++ src/test/java/http/AbstractMessageHeaderTest.java
@@ -0,0 +1,32 @@
+package http;
+
+/**
+ * @author Karl Bennett
+ */
+public abstract class AbstractMessageHeaderTest<M> extends AbstractMessageAttributeTest<M, Header> {
+
+    public AbstractMessageHeaderTest(MessageExecutor<M, Header> messageExecutor) {
+
+        super(new PropertyExecutor<Header>() {
+
+            @Override
+            public <T> Header newProperty(String name, T value) {
+
+                return new Header<T>(name, value);
+            }
+
+            @Override
+            public String getName(Header property) {
+
+                return property.getName();
+            }
+
+            @Override
+            public <T> T getValue(Header property) {
+
+                return (T) property.getValue();
+            }
+
+        }, messageExecutor);
+    }
+}
