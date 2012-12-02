3ab597c3216ff1d67942552a101887580fcf51b0
Karl Bennett <where_1ts@yahoo.com>
2012-12-02 16:57:51 +0000

    Created a new ClientRequestCookieNameAndValueTest that tests the Message.addCookie(String,String) method.

---
+++ src/test/java/http/ClientRequestCookieNameAndValueTest.java
@@ -0,0 +1,50 @@
+package http;
+
+import java.util.Collection;
+
+import static http.Client.Request;
+import static http.Cookies.COOKIE;
+import static http.Urls.TEST_URL;
+
+/**
+ * @author Karl Bennett
+ */
+public class ClientRequestCookieNameAndValueTest extends AbstractMessageCookieTest<Request<Object>> {
+
+
+    public ClientRequestCookieNameAndValueTest() {
+        super(new MessageExecutor<Request<Object>, Cookie>() {
+
+            @Override
+            public Request<Object> newMessage() {
+
+                return new Request<Object>(TEST_URL);
+            }
+
+            @Override
+            public Cookie getProperty(Request<Object> message, String name) {
+
+                return message.getCookie(name);
+            }
+
+            @Override
+            public void addProperty(Request<Object> message, Cookie property) {
+
+                message.addCookie(property.getName(), property.getValue());
+            }
+
+            @Override
+            public Collection<Cookie> getProperties(Request<Object> message) {
+
+                return message.getCookies();
+            }
+
+            @Override
+            public void setProperties(Request<Object> message, Collection<Cookie> properties) {
+
+                message.setCookies(properties);
+            }
+
+        }, COOKIE);
+    }
+}
