9f529c428eb73c83d20a00b4967de00cafe5809e
Karl Bennett <where_1ts@yahoo.com>
2012-12-02 17:07:28 +0000

    Created a new ClientRequestParameterObjectTest class.

---
+++ src/test/java/http/ClientRequestParameterObjectTest.java
@@ -0,0 +1,47 @@
+package http;
+
+import java.util.Collection;
+
+import static http.Client.Request;
+import static http.Urls.TEST_URL;
+
+/**
+ * @author Karl Bennett
+ */
+public class ClientRequestParameterObjectTest extends AbstractClientRequestParameterObjectTest {
+
+    public ClientRequestParameterObjectTest() {
+        super(new MessageExecutor<Request<Object>, Parameter>() {
+
+            @Override
+            public Request<Object> newMessage() {
+
+                return new Request<Object>(TEST_URL);
+            }
+
+            @Override
+            public Parameter getProperty(Request<Object> message, String name) {
+
+                return message.getParameter(name);
+            }
+
+            @Override
+            public void addProperty(Request<Object> message, Parameter property) {
+
+                message.addParameter(property);
+            }
+
+            @Override
+            public Collection<Parameter> getProperties(Request<Object> message) {
+
+                return message.getParameters();
+            }
+
+            @Override
+            public void setProperties(Request<Object> message, Collection<Parameter> properties) {
+
+                message.setParameters(properties);
+            }
+        });
+    }
+}
