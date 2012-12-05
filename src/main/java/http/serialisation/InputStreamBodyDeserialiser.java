1625dee5ad9ea1f1b49901d00633081dcd2c945e
Karl Bennett <where_1ts@yahoo.com>
2012-12-05 22:13:50 +0000

    Updated the comments for the InputStreamBodyDeserialiser class.

--- src/main/java/http/serialisation/InputStreamBodyDeserialiser.java
+++ src/main/java/http/serialisation/InputStreamBodyDeserialiser.java
@@ -7,6 +7,9 @@ import java.io.InputStream;
 /**
  * An abstract class that sets the input type for any child deserialisers to be an {@link InputStream}.
  *
+ * @param <C> the type of {@code Content-Type} the {@code BodyDeserialiser} accepts. This should be set using a typed
+ *            {@link http.Header} like {@link http.header.JsonContentType} or {@link http.header.XmlContentType}.
+ *
  * @author Karl Bennett
  */
 public abstract class InputStreamBodyDeserialiser<C extends ContentType> extends
