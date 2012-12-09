8d7205a0289d563a59064aa3ad38e13b96aa2d9c
Karl Bennett <where_1ts@yahoo.com>
2012-12-09 13:32:31 +0000

    Created a JsonBodyDeserialiserTest class.

---
+++ src/test/java/http/serialisation/JsonBodyDeserialiserTest.java
@@ -0,0 +1,27 @@
+package http.serialisation;
+
+import http.header.JsonContentType;
+
+import static http.serialisation.AbstractBodyDeserialiserTester.TestDeserialisedObject;
+import static org.apache.commons.io.IOUtils.toInputStream;
+
+/**
+ * @author Karl Bennett
+ */
+public class JsonBodyDeserialiserTest extends AbstractBodyDeserialiserTester<JsonContentType, TestDeserialisedObject> {
+
+    private static final String SERIALISED_VALUE = "{" +
+                "\"id\":" + ID + "," +
+                "\"name\":" + NAME + "," +
+                "\"friends\":[" +
+                    FRIEND_ONE + "," +
+                    FRIEND_TWO + "," +
+                    FRIEND_THREE +
+                "]" +
+            "}";
+
+
+    protected JsonBodyDeserialiserTest() {
+        super(new JsonBodyDeserialiser(), toInputStream(SERIALISED_VALUE), TEST_DESERIALISED_OBJECT);
+    }
+}
