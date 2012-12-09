aed53d3a18af21460b2aa6199b33b4fa993775e6
Karl Bennett <where_1ts@yahoo.com>
2012-12-09 11:35:15 +0000

    Created a new ClientResponseCookieNameAndValueTest class.

---
+++ src/test/java/http/ClientResponseCookieNameAndValueTest.java
@@ -0,0 +1,50 @@
+package http;
+
+import java.util.Collection;
+
+import static http.Client.Response;
+import static http.Cookies.SET_COOKIE;
+import static java.net.HttpURLConnection.HTTP_OK;
+
+/**
+ * @author Karl Bennett
+ */
+public class ClientResponseCookieNameAndValueTest extends AbstractMessageCookieTest<Response<Object>> {
+
+
+    public ClientResponseCookieNameAndValueTest() {
+        super(new MessageExecutor<Response<Object>, Cookie>() {
+
+            @Override
+            public Response<Object> newMessage() {
+
+                return new Response<Object>(HTTP_OK);
+            }
+
+            @Override
+            public Cookie getProperty(Response<Object> message, String name) {
+
+                return message.getCookie(name);
+            }
+
+            @Override
+            public void addProperty(Response<Object> message, Cookie property) {
+
+                message.addCookie(property.getName(), property.getValue());
+            }
+
+            @Override
+            public Collection<Cookie> getProperties(Response<Object> message) {
+
+                return message.getCookies();
+            }
+
+            @Override
+            public void setProperties(Response<Object> message, Collection<Cookie> properties) {
+
+                message.setCookies(properties);
+            }
+
+        }, SET_COOKIE);
+    }
+}
