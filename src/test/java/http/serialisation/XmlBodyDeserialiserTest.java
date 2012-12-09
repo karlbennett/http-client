207a7fcacfcace8bba6e4d2430ecd2294c73783d
Karl Bennett <where_1ts@yahoo.com>
2012-12-09 13:37:16 +0000

    Created a XmlBodyDeserialiserTest class.

---
+++ src/test/java/http/serialisation/XmlBodyDeserialiserTest.java
@@ -0,0 +1,26 @@
+package http.serialisation;
+
+import http.header.XmlContentType;
+
+import static http.serialisation.AbstractBodyDeserialiserTester.TestDeserialisedObject;
+import static org.apache.commons.io.IOUtils.toInputStream;
+
+/**
+ * @author Karl Bennett
+ */
+public class XmlBodyDeserialiserTest extends AbstractBodyDeserialiserTester<XmlContentType, TestDeserialisedObject> {
+
+    private static final String SERIALISED_VALUE = "<test>" +
+                "<id>" + ID + "</id>" +
+                "<name>" + NAME + "</name>" +
+                "<friends>" +
+                    "<friend>" + FRIEND_ONE + "</friend>" +
+                    "<friend>" + FRIEND_TWO + "</friend>" +
+                    "<friend>" + FRIEND_THREE + "</friend>" +
+                "</friends>" +
+            "</test>";
+
+    protected XmlBodyDeserialiserTest() {
+        super(new XmlBodyDeserialiser(), toInputStream(SERIALISED_VALUE), TEST_DESERIALISED_OBJECT);
+    }
+}
