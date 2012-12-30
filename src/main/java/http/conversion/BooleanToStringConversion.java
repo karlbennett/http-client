b1a7821cc861138af3fbbe97037763b434f0dc07
karlbennett <where_1ts@yahoo.com>
2012-12-30 19:14:03 +0000

    Updated the comments in the BooleanToStringConversion class.

--- src/main/java/http/conversion/BooleanToStringConversion.java
+++ src/main/java/http/conversion/BooleanToStringConversion.java
@@ -18,7 +18,8 @@ public class BooleanToStringConversion extends Conversion<Boolean, String> {
      * Convert a {@code Boolean} into a {@code String}.
      *
      * @param input the {@code Boolean} that will be converted.
-     * @return the {@code String} "true" if the {@code Boolean} was {@code true}, otherwise the {@code String} "false".
+     * @return the {@code String} "true" if the {@code Boolean} was {@code true}, otherwise the {@code String} "false"
+     *         or an empty {@code String} if the input is {@code null}.
      */
     @Override
     public String convert(Boolean input) {
