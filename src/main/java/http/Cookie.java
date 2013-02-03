c6fe231cb273417e21220451bafb61c232f33b51
karlbennett <where_1ts@yahoo.com>
2013-02-03 19:39:22 +0000

    Implemented the Cookie class.

--- src/main/java/http/Cookie.java
+++ src/main/java/http/Cookie.java
@@ -1,8 +1,23 @@
 package http;
 
+import http.attribute.Attribute;
+import http.date.DateParseException;
+
 import java.net.URI;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static http.util.Asserts.assertNotNull;
+import static http.util.Checks.isNotNull;
+import static http.util.Checks.isNull;
+import static http.util.URIs.quietUri;
 
 /**
  * Represents an HTTP cookie, that is a header with the name {@code Cookie} and a value containing the cookie name and
@@ -14,7 +29,93 @@ import java.util.Date;
  * @author Karl Bennett
  * @see <a href="http://www.ietf.org/rfc/rfc2109.txt">RFC 2109</a>
  */
-public class Cookie {
+public class Cookie extends Attribute<String> {
+
+    private static final ThreadLocal<DateFormat> DATE_FORMAT_THREAD_LOCAL = new ThreadLocal<DateFormat>() {
+
+        @Override
+        protected DateFormat initialValue() {
+            return new SimpleDateFormat("E',' dd MMM yyyy HH':'mm':'ss z");
+        }
+    };
+
+    public static final String COMMENT = "Comment";
+    public static final String DOMAIN = "Domain";
+    public static final String EXPIRES = "Expires";
+    public static final String MAX_AGE = "Max-Age";
+    public static final String PATH = "Path";
+    public static final String VERSION = "Version";
+    public static final String SECURE = "Secure";
+
+    private static final Pattern COOKIE_REGEX = Pattern.compile(
+            "(?i)(?:^|\\s)([^\\s\\(\\)\\[\\]\\{\\}=,\"\"\\\\/?@:;]+)=([^\\s\\(\\)\\[\\]\\{\\}=,\"\"\\\\/?@:;]+)" +
+                    "(?:" +
+                    /**/";\\s+(" + COMMENT + "=[^\\(\\)\\[\\]\\{\\}=,\"\"\\\\/?@:;]+)|" +
+                    /**/";\\s+(" + DOMAIN + "=[^\\s\\(\\)\\[\\]\\{\\}=,\"\"\\\\@:;]+)|" +
+                    /**/";\\s+(" + EXPIRES + "=[^\\(\\)\\[\\]\\{\\}=\"\"\\\\/?@;]+)|" +
+                    /**/";\\s+(" + MAX_AGE + "=\\d+)|" +
+                    /**/";\\s+(" + PATH + "=[^\\s\\(\\)\\[\\]\\{\\}=,\"\"\\\\?@:;]+)|" +
+                    /**/";\\s+(" + VERSION + "=[\\d]+)|" +
+                    /**/";\\s+(" + SECURE + ")|" +
+                    /**/"(this is here to force the Secure capture. Don't know why it's needed.)" +
+                    ")*");
+
+
+    private static Date parseDate(String date) {
+
+        assertNotNull("date", date);
+
+        try {
+
+            return DATE_FORMAT_THREAD_LOCAL.get().parse(date);
+
+        } catch (ParseException e) {
+
+            throw new DateParseException(e);
+        }
+    }
+
+    private static String formatDate(Date date) {
+
+        return DATE_FORMAT_THREAD_LOCAL.get().format(date);
+    }
+
+    /**
+     * Parse the supplied {@code Cookie} or {@code Set-Cookie} header into the cookies contained within its value.
+     *
+     * @param value the value of a {@code Cookie} or {@code Set-Cookie} header that will be parsed into one or more
+     *              cookies.
+     * @return a collection of all the cookies contained within the header.
+     * @throws IllegalArgumentException if the supplied header is not a {@code Cookie} header.
+     */
+    public static Collection<Cookie> parse(String value) {
+
+        Matcher matcher = COOKIE_REGEX.matcher(value);
+
+        Set<Cookie> cookies = new HashSet<>();
+
+        Cookie cookie;
+
+        while (matcher.find()) {
+
+            cookie = new Cookie(matcher.group(1), matcher.group(2));
+
+            for (int i = 3; i < matcher.groupCount(); i++) cookie.setFieldByFragment(matcher.group(i));
+
+            cookies.add(cookie);
+        }
+
+        return cookies;
+    }
+
+
+    private String comment;
+    private String domain;
+    private Date expires;
+    private int maxAge;
+    private URI path;
+    private boolean secure;
+    private int version;
 
     /**
      * Create a new {@code Cookie} with the supplied name and value.
@@ -23,7 +124,11 @@ public class Cookie {
      * @param value the value of the cookie.
      */
     public Cookie(String name, String value) {
+        super(name, value);
 
+        assertNotNull("value", value);
+
+        this.maxAge = -1; // Initialise maxAge to -1 because 0 indicates an expired value.
     }
 
     /**
@@ -31,7 +136,7 @@ public class Cookie {
      */
     public String getComment() {
 
-        return null;
+        return comment;
     }
 
     /**
@@ -41,6 +146,7 @@ public class Cookie {
      */
     public void setComment(String comment) {
 
+        this.comment = comment;
     }
 
     /**
@@ -48,7 +154,7 @@ public class Cookie {
      */
     public String getDomain() {
 
-        return null;
+        return domain;
     }
 
     /**
@@ -58,6 +164,7 @@ public class Cookie {
      */
     public void setDomain(String domain) {
 
+        this.domain = domain;
     }
 
     /**
@@ -65,7 +172,7 @@ public class Cookie {
      */
     public Date getExpires() {
 
-        return null;
+        return expires;
     }
 
     /**
@@ -77,6 +184,7 @@ public class Cookie {
      */
     public void setExpires(String expires) {
 
+        this.expires = parseDate(expires);
     }
 
     /**
@@ -86,6 +194,7 @@ public class Cookie {
      */
     public void setExpires(Date expires) {
 
+        this.expires = expires;
     }
 
     /**
@@ -93,7 +202,7 @@ public class Cookie {
      */
     public boolean hasExpired() {
 
-        return false;
+        return 0 == maxAge || (isNotNull(expires) && new Date().getTime() > expires.getTime());
     }
 
     /**
@@ -101,24 +210,17 @@ public class Cookie {
      */
     public int getMaxAge() {
 
-        return 0;
+        return maxAge;
     }
 
     /**
      * Set the maximum age of the cookie as the number of seconds from the current time stamp.
      *
-     * @param expiry how long till the cookie expires.
+     * @param maxAge how long till the cookie expires.
      */
-    public void setMaxAge(int expiry) {
+    public void setMaxAge(int maxAge) {
 
-    }
-
-    /**
-     * @return the name of the cookie.
-     */
-    public String getName() {
-
-        return null;
+        this.maxAge = maxAge;
     }
 
     /**
@@ -126,25 +228,27 @@ public class Cookie {
      */
     public URI getPath() {
 
-        return null;
+        return path;
     }
 
     /**
      * Set the path of the cookie.
      *
-     * @param uri a {@code String} containing the new cookie path.
+     * @param path a {@code String} containing the new cookie path.
      */
-    public void setPath(String uri) {
+    public void setPath(String path) {
 
+        this.path = isNull(path) ? null : quietUri(path);
     }
 
     /**
      * Set the path of the cookie.
      *
-     * @param uri the new cookie path.
+     * @param path the new cookie path.
      */
-    public void setPath(URI uri) {
+    public void setPath(URI path) {
 
+        this.path = path;
     }
 
     /**
@@ -152,24 +256,17 @@ public class Cookie {
      */
     public boolean isSecure() {
 
-        return false;
+        return secure;
     }
 
     /**
      * Set if this is a secure cookie or not.
      *
-     * @param flag true if the cookie is secure otherwise false.
+     * @param secure true if the cookie is secure otherwise false.
      */
-    public void setSecure(boolean flag) {
+    public void setSecure(boolean secure) {
 
-    }
-
-    /**
-     * @return the value of the cookie.
-     */
-    public String getValue() {
-
-        return null;
+        this.secure = secure;
     }
 
     /**
@@ -177,7 +274,7 @@ public class Cookie {
      */
     public int getVersion() {
 
-        return 0;
+        return version;
     }
 
     /**
@@ -187,17 +284,114 @@ public class Cookie {
      */
     public void setVersion(int version) {
 
+        this.version = version;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        if (!super.equals(o)) return false;
+
+        Cookie cookie = (Cookie) o;
+
+        return maxAge == cookie.maxAge && secure == cookie.secure && version == cookie.version &&
+                !(comment != null ? !comment.equals(cookie.comment) : cookie.comment != null) &&
+                !(domain != null ? !domain.equals(cookie.domain) : cookie.domain != null) &&
+                !(expires != null ? !expires.equals(cookie.expires) : cookie.expires != null) &&
+                !(path != null ? !path.equals(cookie.path) : cookie.path != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+
+        result = 31 * result + (comment != null ? comment.hashCode() : 0);
+
+        result = 31 * result + (domain != null ? domain.hashCode() : 0);
+
+        result = 31 * result + (expires != null ? expires.hashCode() : 0);
+
+        result = 31 * result + maxAge;
+
+        result = 31 * result + (path != null ? path.hashCode() : 0);
+
+        result = 31 * result + (secure ? 1 : 0);
+
+        result = 31 * result + version;
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder toStringBuilder = new StringBuilder(getName()).append('=').append(getValue());
+
+        if (isNotNull(comment)) toStringBuilder.append("; ").append(COMMENT).append("=").append(comment);
+
+        if (isNotNull(domain)) toStringBuilder.append("; ").append(DOMAIN).append("=").append(domain);
+
+        if (isNotNull(expires)) toStringBuilder.append("; ").append(EXPIRES).append("=").append(formatDate(expires));
+
+        if (0 < maxAge) toStringBuilder.append("; ").append(MAX_AGE).append("=").append(maxAge);
+
+        if (isNotNull(path)) toStringBuilder.append("; ").append(PATH).append("=").append(path);
+
+        if (0 < version) toStringBuilder.append("; ").append(VERSION).append("=").append(version);
+
+        if (secure) toStringBuilder.append("; ").append(SECURE);
+
+        return toStringBuilder.toString();
     }
 
     /**
-     * Parse the supplied {@code Cookie} header into the cookies contained within its value.
+     * A private helper method for setting the field of this cookie from the fragment of a cookie string.
      *
-     * @param value the value of a {@code Cookie} header that will be parsed into one or more cookies.
-     * @return a collection of all the cookies contained within the header.
-     * @throws IllegalArgumentException if the supplied header is not a {@code Cookie} header.
+     * Example:
+     * <code>
+     *      Cookie cookie = new Cookie("name", "value");
+     *      cookie.setFieldByFragment("Comment=This is a comment;");
+     *      "This is a comment".equals(cookie.getComment()); // true
+     * </code>
+     *
+     * @param fragment the fragment of the cookie string that is going to be used to set a field.
      */
-    public static Collection<Cookie> parse(String value) {
+    private void setFieldByFragment(String fragment) {
 
-        return null;
+        // Special case for the "Secure" cookie attribute.
+        if (SECURE.equals(fragment)) {
+
+            setSecure(true);
+
+            return;
+        }
+
+        String[] parts = fragment.split("=");
+
+        if (2 > parts.length) {
+
+            throw new IllegalArgumentException(
+                    "The (fragment) variable must have a structure of (\\w+=\\w+) or equal \"Secure\".");
+        }
+
+        // Set the appropriate field.
+        // Yes this could be done with reflection, but that would be error prone, slow, and over kill.
+        if (parts[0].startsWith(COMMENT)) setComment(parts[1]);
+
+        if (parts[0].startsWith(DOMAIN)) setDomain(parts[1]);
+
+        if (parts[0].startsWith(EXPIRES)) setExpires(parts[1]);
+
+        if (parts[0].startsWith(MAX_AGE)) setMaxAge(Integer.parseInt(parts[1]));
+
+        if (parts[0].startsWith(PATH)) setPath(parts[1]);
+
+        if (parts[0].startsWith(VERSION)) setVersion(Integer.parseInt(parts[1]));
     }
 }
--- src/test/java/http/CookieTest.java
+++ src/test/java/http/CookieTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,20 +22,21 @@ public class CookieTest {
 
     public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormat.forPattern("E',' dd MMM yyyy HH':'mm':'ss z");
 
-    public static final String NAME = "cookie_name";
+    public static final String NAME_ONE = "cookie_name_one";
+    public static final String NAME_TWO = "cookie_name_two";
     public static final String VALUE = "cookie_value";
-    public static final Pattern NAME_REGEX = Pattern.compile("(" + NAME + ")=" + VALUE + ";");
-    public static final Pattern VALUE_REGEX = Pattern.compile(NAME + "=(" + VALUE + ");");
-    public static final Pattern COMMENT_REGEX = Pattern.compile("Comment=(\\w+);");
+    public static final Pattern NAME_REGEX = Pattern.compile("(" + NAME_ONE + ")=" + VALUE);
+    public static final Pattern VALUE_REGEX = Pattern.compile(NAME_ONE + "=(" + VALUE + ")");
+    public static final Pattern COMMENT_REGEX = Pattern.compile(";\\s+Comment=(\\w+)");
     public static final String COMMENT = "cookie_comment";
-    public static final Pattern DOMAIN_REGEX = Pattern.compile("Domain=(\\w+);");
+    public static final Pattern DOMAIN_REGEX = Pattern.compile(";\\s+Domain=(\\w+)");
     public static final String DOMAIN = "www.cookie.com";
-    public static final Pattern EXPIRES_REGEX = Pattern.compile("Expires=([\\w\\d,; ]+);");
-    public static final String EXPIRES = DATE_FORMATTER.print(DateTime.now());
-    public static final Pattern MAX_AGE_REGEX = Pattern.compile("Max-Age=(\\d+);");
+    public static final Pattern EXPIRES_REGEX = Pattern.compile(";\\s+Expires=([\\w\\s,:]+)");
+    public static final DateTime EXPIRES_DATE = new DateTime(DateTime.now().getMillis() / 1000 * 1000);
+    public static final String EXPIRES = DATE_FORMATTER.print(EXPIRES_DATE);
+    public static final Pattern MAX_AGE_REGEX = Pattern.compile(";\\s+Max-Age=(\\d+)");
     public static final int MAX_AGE = (int) ((DateTime.now().plusDays(1).getMillis() - DateTime.now().getMillis()) / 1000);
-    public static final Pattern PATH_REGEX = Pattern.compile("Path=((?:https?://)?[\\w/\\._-]);");
-    public static final String PATH_NAME = "Path";
+    public static final Pattern PATH_REGEX = Pattern.compile(";\\s+Path=((?:https?://)?[\\w/\\._-]+)");
     public static final URI PATH;
     static {
 
@@ -56,13 +58,16 @@ public class CookieTest {
     public static final Pattern VERSION_REGEX = Pattern.compile("Version=(\\d+);");
     public static final int VERSION = 1;
 
-    public static final String COOKIE_STRING = NAME + "=" + VALUE + "; Comment=" + COMMENT + "; Domain=" + DOMAIN
-            + "; Expires=" + EXPIRES + "; Max-Age=" + MAX_AGE + "; Path=" + PATH + "; Secure=" + SECURE + "; Version=" + VERSION;
+    public static final String COOKIE_STRING_ONE = NAME_ONE + "=" + VALUE + "; Domain=" + DOMAIN + "; Comment=" + COMMENT
+            + "; Max-Age=" + MAX_AGE + "; Path=" + PATH + "; Expires=" + EXPIRES + "; Secure" + "; Version=" + VERSION;
+
+    public static final String COOKIE_STRING_TWO = NAME_TWO + "=" + VALUE + "; Domain=" + DOMAIN + "; Max-Age=" + MAX_AGE
+            + "; Comment=" + COMMENT + "; Version=" + VERSION + "; Path=" + PATH + "; Secure" + "; Expires=" + EXPIRES;
 
     @Test
     public void testCreateCookie() throws Exception {
 
-        String cookieString = new Cookie(NAME, VALUE).toString();
+        String cookieString = new Cookie(NAME_ONE, VALUE).toString();
 
         Matcher matcher = NAME_REGEX.matcher(cookieString);
         assertTrue("the name of the cookie should be in the cookie string.", matcher.find());
@@ -99,7 +104,7 @@ public class CookieTest {
     @Test(expected = IllegalArgumentException.class)
     public void testCreateCookieWithNullValue() throws Exception {
 
-        new Cookie(NAME, null);
+        new Cookie(NAME_ONE, null);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -111,11 +116,10 @@ public class CookieTest {
     @Test
     public void testSetExpiresWithString() throws Exception {
 
-        Cookie cookie = new Cookie(NAME, VALUE);
+        Cookie cookie = new Cookie(NAME_ONE, VALUE);
         cookie.setExpires(EXPIRES);
 
-        assertEquals("the expries date should be set correctly.", DATE_FORMATTER.parseDateTime(EXPIRES).toDate(),
-                cookie.getExpires());
+        assertEquals("the expries date should be set correctly.", EXPIRES_DATE.toDate(), cookie.getExpires());
 
         String cookieString = cookie.toString();
 
@@ -143,21 +147,21 @@ public class CookieTest {
     @Test(expected = DateParseException.class)
     public void testSetExpiresWithMalformedString() throws Exception {
 
-        Cookie cookie = new Cookie(NAME, VALUE);
+        Cookie cookie = new Cookie(NAME_ONE, VALUE);
         cookie.setExpires("malformed date");
     }
 
-    @Test(expected = DateParseException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testSetExpiresWithNullString() throws Exception {
 
-        Cookie cookie = new Cookie(NAME, VALUE);
+        Cookie cookie = new Cookie(NAME_ONE, VALUE);
         cookie.setExpires((String) null);
     }
 
     @Test
     public void testHasExpired() throws Exception {
 
-        Cookie cookie = new Cookie(NAME, VALUE);
+        Cookie cookie = new Cookie(NAME_ONE, VALUE);
         cookie.setExpires(DateTime.now().minusMinutes(1).toDate());
 
         assertTrue("the cookie should have expired.", cookie.hasExpired());
@@ -166,7 +170,7 @@ public class CookieTest {
     @Test
     public void testHasExpiredWithMaxAge() throws Exception {
 
-        Cookie cookie = new Cookie(NAME, VALUE);
+        Cookie cookie = new Cookie(NAME_ONE, VALUE);
         cookie.setMaxAge(0);
 
         assertTrue("the cookie should have expired.", cookie.hasExpired());
@@ -175,7 +179,7 @@ public class CookieTest {
     @Test
     public void testHasExpiredWithFutureDate() throws Exception {
 
-        Cookie cookie = new Cookie(NAME, VALUE);
+        Cookie cookie = new Cookie(NAME_ONE, VALUE);
         cookie.setExpires(DateTime.now().plusDays(1).toDate());
 
         assertFalse("the cookie should not have expired.", cookie.hasExpired());
@@ -184,7 +188,7 @@ public class CookieTest {
     @Test
     public void testHasExpiredWithFutureMaxAge() throws Exception {
 
-        Cookie cookie = new Cookie(NAME, VALUE);
+        Cookie cookie = new Cookie(NAME_ONE, VALUE);
         cookie.setMaxAge((int) (DateTime.now().plusDays(1).getMillis() / 1000));
 
         assertFalse("the cookie should not have expired.", cookie.hasExpired());
@@ -193,7 +197,11 @@ public class CookieTest {
     @Test
     public void testSetPath() throws Exception {
 
-        Cookie cookie = new Cookie(NAME, VALUE);
+        Cookie cookie = new Cookie(NAME_ONE, VALUE);
+        cookie.setPath("");
+
+        assertEquals("the path URI should be set correctly.", "", cookie.getPath().toString());
+
         cookie.setPath(PATH.toString());
 
         assertEquals("the path URI should be set correctly.", PATH, cookie.getPath());
@@ -221,43 +229,55 @@ public class CookieTest {
         assertFalse("the verion of the cookie should not be in the cookie string.", matcher.find());
     }
 
-    @Test(expected = URIException.class)
+    @Test
     public void testSetNullPath() throws Exception {
 
-        Cookie cookie = new Cookie(NAME, VALUE);
+        Cookie cookie = new Cookie(NAME_ONE, VALUE);
         cookie.setPath((String) null);
 
-        assertNull("the path URI should be set ti null correctly.", cookie.getPath());
+        assertNull("the path URI should be set to null correctly.", cookie.getPath());
     }
 
     @Test
     public void testGetName() throws Exception {
 
-        assertEquals("the name should be set correctly.", NAME, new Cookie(NAME, VALUE).getName());
+        assertEquals("the name should be set correctly.", NAME_ONE, new Cookie(NAME_ONE, VALUE).getName());
     }
 
     @Test
     public void testGetValue() throws Exception {
 
-        assertEquals("the name should be set correctly.", VALUE, new Cookie(NAME, VALUE).getValue());
+        assertEquals("the name should be set correctly.", VALUE, new Cookie(NAME_ONE, VALUE).getValue());
     }
 
     @Test
     public void testParse() throws Exception {
 
-        Cookie cookie = new Cookie(NAME, VALUE);
-        cookie.setComment(COMMENT);
-        cookie.setDomain(DOMAIN);
-        cookie.setExpires(EXPIRES);
-        cookie.setMaxAge(MAX_AGE);
-        cookie.setPath(PATH);
-        cookie.setSecure(SECURE);
-        cookie.setVersion(VERSION);
+        Cookie cookieOne = new Cookie(NAME_ONE, VALUE);
+        cookieOne.setComment(COMMENT);
+        cookieOne.setDomain(DOMAIN);
+        cookieOne.setExpires(EXPIRES);
+        cookieOne.setMaxAge(MAX_AGE);
+        cookieOne.setPath(PATH);
+        cookieOne.setSecure(SECURE);
+        cookieOne.setVersion(VERSION);
 
-        Collection<Cookie> parsedCookies = Cookie.parse(COOKIE_STRING);
+        Cookie cookieTwo = new Cookie(NAME_TWO, VALUE);
+        cookieTwo.setComment(COMMENT);
+        cookieTwo.setDomain(DOMAIN);
+        cookieTwo.setExpires(EXPIRES);
+        cookieTwo.setMaxAge(MAX_AGE);
+        cookieTwo.setPath(PATH);
+        cookieTwo.setSecure(SECURE);
+        cookieTwo.setVersion(VERSION);
+
+        Collection<Cookie> parsedCookies = Cookie.parse(COOKIE_STRING_ONE + "; " + COOKIE_STRING_TWO);
 
         assertNotNull("a collection of cookies should be produced.", parsedCookies);
-        assertEquals("a single cookie should be pasred.", 1, parsedCookies.size());
-        assertEquals("the pasred cookie should contain the correct values.", cookie, parsedCookies.iterator().next());
+        assertEquals("two cookies should be parsed.", 2, parsedCookies.size());
+
+        Iterator<Cookie> iterator = parsedCookies.iterator();
+        assertEquals("the parsed cookieOne should contain the correct values.", cookieOne, iterator.next());
+        assertEquals("the parsed cookieTwo should contain the correct values.", cookieTwo, iterator.next());
     }
 }
