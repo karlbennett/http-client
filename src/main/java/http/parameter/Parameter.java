55fd783480c90012a09bb95b05bcdc9ef4484949
karlbennett <where_1ts@yahoo.com>
2013-02-04 01:37:55 +0000

    Created new Parameter.toString(Collection<Parameter>) and Parameter.parse(String) methods.

--- src/main/java/http/parameter/Parameter.java
+++ src/main/java/http/parameter/Parameter.java
@@ -2,7 +2,12 @@ package http.parameter;
 
 import http.attribute.MultiValueAttribute;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+
+import static http.util.Checks.isEmpty;
 
 /**
  * Represents an HTTP parameter which can have a name and a single or multiple values. The type of the value can be
@@ -11,6 +16,46 @@ import java.util.List;
  * @author Karl Bennett
  */
 public class Parameter<T> extends MultiValueAttribute<T> {
+
+    public static String toString(Collection<Parameter> parameters) {
+
+        if (isEmpty(parameters)) return "";
+
+        StringBuilder toStringHolder = new StringBuilder();
+
+        for (Parameter parameter : parameters) {
+
+            toStringHolder.append(parameter).append('&');
+        }
+
+        toStringHolder.replace(toStringHolder.length() - 1, toStringHolder.length(), "");
+
+        return toStringHolder.toString();
+    }
+
+    public static Collection<Parameter> parse(String query) {
+
+        if (isEmpty(query)) return Collections.emptySet();
+
+        Collection<Parameter>  parameters = new HashSet<>();
+
+        String[] parameterParts;
+        for (String parameter : query.split("&")) {
+
+            parameterParts = parameter.split("=");
+
+            if (2 == parameterParts.length) {
+
+                parameters.add(new Parameter<>(parameterParts[0], parameterParts[0]));
+
+            } else {
+
+                throw new IllegalStateException("Invalid parameter format (" + parameter + ")");
+            }
+        }
+
+        return parameters;
+    }
 
     /**
      * Create an {@code Parameter} with a name and multiple values.
@@ -30,5 +75,12 @@ public class Parameter<T> extends MultiValueAttribute<T> {
      */
     public Parameter(String name, T value) {
         super(name, value);
+    }
+
+
+    @Override
+    public String toString() {
+
+        return super.toString("&");
     }
 }
