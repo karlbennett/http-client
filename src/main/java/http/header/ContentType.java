65896264b14b521d30e8d416526cd1af8cf7dd51
karlbennett <where_1ts@yahoo.com>
2013-02-24 20:13:47 +0000

    Added a ContentType(String,String) constructor to the ContentType class.

--- src/main/java/http/header/ContentType.java
+++ src/main/java/http/header/ContentType.java
@@ -2,6 +2,8 @@ package http.header;
 
 import javax.activation.MimeType;
 
+import static http.util.MimeTypes.quietMimeType;
+
 /**
  * An HTTP {@code Content-Type} header.
  *
@@ -9,14 +11,24 @@ import javax.activation.MimeType;
  */
 public class ContentType extends Header<MimeType> {
 
-    public static final String NAME = "Content-Type";
+    public static final String CONTENT_TYPE = "Content-Type";
 
     /**
-     * Create a new {@code ContentType} header with the supplied MIME type.
+     * Create a new {@code ContentType} header with the supplied {@code MIME} type.
      *
-     * @param mimeType the MIME type to be used for the HTTP {@code Content-Type}.
+     * @param mimeType the {@code MIME} type to be used for the HTTP {@code Content-Type}.
      */
     public ContentType(MimeType mimeType) {
-        super(NAME, mimeType);
+        super(CONTENT_TYPE, mimeType);
+    }
+
+    /**
+     * Create a new {@code ContentType} header with the supplied {@code MIME} type components.
+     *
+     * @param primary the primary part of the {@code MIME} type e.g. "application" from "application/json".
+     * @param sub     the sub type of the {@code MIME} type e.g. "json" from "application/json".
+     */
+    public ContentType(String primary, String sub) {
+        super(CONTENT_TYPE, quietMimeType(primary, sub));
     }
 }
