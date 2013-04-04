98975e0d26f427155a224f31a2bb89b715870729
karlbennett <where_1ts@yahoo.com>
2013-04-04 23:02:42 +0100

    Implemented the Cookie and SetCookie convert methods.

--- src/main/java/http/header/Cookie.java
+++ src/main/java/http/header/Cookie.java
@@ -1,5 +1,11 @@
 package http.header;
 
+import http.util.NullSafeForEach;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * An HTTP {@code Cookie} header.
  *
@@ -8,6 +14,40 @@ package http.header;
 public class Cookie extends Header<http.Cookie> {
 
     public static final String COOKIE = "Cookie";
+
+    /**
+     * Convert the supplied {@link Header} into a {@link List} of {@code Cookie} instances.
+     *
+     * @param header the {@code Header} to convert.
+     * @return the {@code List} of {@code Cookie} instances built from the {@code Header}.
+     * @throws IllegalArgumentException if the supplied header doesn't have a name of "Cookie" or the value doesn't
+     *                                  contain valid HTTP cookies.
+     */
+    public static List<Cookie> convert(Header header) {
+
+        if (header instanceof Cookie) {
+
+            return Collections.singletonList((Cookie) header);
+        }
+
+        if (COOKIE.equals(header.getName())) {
+
+            return new ArrayList<Cookie>(
+                    new NullSafeForEach<http.Cookie, Cookie>(http.Cookie.parse(header.getValue().toString())) {
+
+                        @Override
+                        protected Cookie next(http.Cookie element) {
+
+                            return new Cookie(element);
+                        }
+
+                    }.results()
+            );
+        }
+
+        throw new IllegalArgumentException("An " + Cookie.class.getName() +
+                " object can only be created from an \"Cookie\" header.");
+    }
 
     /**
      * Create a {@code Cookie} header with the supplied {@link http.Cookie}.
--- src/main/java/http/header/SetCookie.java
+++ src/main/java/http/header/SetCookie.java
@@ -1,6 +1,11 @@
 package http.header;
 
 import http.Cookie;
+import http.util.NullSafeForEach;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * An HTTP {@code Set-Cookie} header.
@@ -10,6 +15,40 @@ import http.Cookie;
 public class SetCookie extends Header<Cookie> {
 
     public static final String SET_COOKIE = "Set-Cookie";
+
+    /**
+     * Convert the supplied {@link Header} into a {@link java.util.List} of {@code SetCookie} instances.
+     *
+     * @param header the {@code Header} to convert.
+     * @return the {@code List} of {@code SetCookie} instances built from the {@code Header}.
+     * @throws IllegalArgumentException if the supplied header doesn't have a name of "Set-Cookie" or the value doesn't
+     *                                  contain valid HTTP cookies.
+     */
+    public static List<SetCookie> convert(Header header) {
+
+        if (header instanceof SetCookie) {
+
+            return Collections.singletonList((SetCookie) header);
+        }
+
+        if (SET_COOKIE.equals(header.getName())) {
+
+            return new ArrayList<SetCookie>(
+                    new NullSafeForEach<Cookie, SetCookie>(Cookie.parse(header.getValue().toString())) {
+
+                        @Override
+                        protected SetCookie next(Cookie element) {
+
+                            return new SetCookie(element);
+                        }
+
+                    }.results()
+            );
+        }
+
+        throw new IllegalArgumentException("An " + SetCookie.class.getName() +
+                " object can only be created from an \"Set-Cookie\" header.");
+    }
 
     /**
      * Create a {@code SetCookie} header with the supplied {@link Cookie} value.
